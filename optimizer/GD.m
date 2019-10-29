20b90a37fd9465e134b5fd7f30ea11fd6abcc10d
Kitsunetic <kitsune.guru@gmail.com>
2019-10-29 16:07:17 +0900

    Remove fprintf of GD

--- optimizer/GD.m
+++ optimizer/GD.m
@@ -1,6 +1,5 @@
 function [fin, x] = GD(f, x, a, eps)
     grad = (f(x + a) - f(x - a)) / (2*a);
-    fprintf("x=%f, grad=%f\n", x, grad);
-    fin = abs(grad) <= eps; 
+    fin = abs(grad) <= eps;
     x = x - a * grad;
 end
