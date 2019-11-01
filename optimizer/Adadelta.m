10b70d1ddfc4e0f4b7f393ef87ed7d07b4493866
Kitsunetic <kitsune.guru@gmail.com>
2019-11-01 12:54:14 +0900

    Update Adadelta

--- optimizer/Adadelta.m
+++ optimizer/Adadelta.m
@@ -1,6 +1,5 @@
-function [fin, k, x] = Adadelta(f, x, a, k, eps, lambda)
-    % Adadelta
-    % Adadelta [22] is an extension of Adagrad that seeks to reduce its aggressive, monotonically decreasing
+function [fin, k, x] = Adadelta(f, x, a, k, eps, lambda, gamma)
+    % Adadelta is an extension of Adagrad that seeks to reduce its aggressive, monotonically decreasing
     % learning rate. Instead of accumulating all past squared gradients, Adadelta restricts the window of
     % accumulated past gradients to some fixed size w.
     
@@ -12,15 +11,18 @@ function [fin, k, x] = Adadelta(f, x, a, k, eps, lambda)
     % lambda - percent parameter
     
     persistent G;
+    persistent H;
     
-    if k == 0
-        G = [x.^2];
-    else
-        G(k+1) = x.^2;
-    end
     grad = (f(x+a) - f(x-a)) / (2*a);
+    if k == 0
+        G = grad.^2;
+        H = a.^2/(G+eps) * grad.^2;
+    else
+        G = gamma*G + (1-gamma)*grad.^2;
+        H = gamma*H + (1-gamma)*(a.^2/(G+eps) * grad.^2);
+    end
     
     fin = abs(grad) <= eps;
-    x = x - a/sqrt(sum(G)+eps) * grad;
+    x = x - sqrt(H+eps)/sqrt(G+eps)*grad;
     k = k + 1;
 end
