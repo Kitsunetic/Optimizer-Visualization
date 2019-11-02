5cb29a1e70817ad9aa69045a2b07e4b60c86de39
Kitsunetic <kitsune.guru@gmail.com>
2019-11-02 16:24:41 +0900

    Update Adam optimizer parameters

--- optimizer/Adam.m
+++ optimizer/Adam.m
@@ -1,25 +1,27 @@
-function [fin, k, x] = Adam(f, x, a, k, eps, beta1, beta2)
+function [fin, x] = Adam(f, x, a, eps, beta1, beta2)
     % Adam Optimizer
     % f - function
     % x - variable(theta in paper)
     % a - step size
-    % k - current index
     % eps - epsilon
     % beta1 - 
     % beta2 - 
     
+    persistent k;
     persistent m;
     persistent v;
     persistent beta1_;
     persistent beta2_;
     
     grad = (f(x+a) - f(x-a)) / (2*a);
-    if k == 0
+    if isempty(k)
+        k = 1;
         m = grad;
         v = grad.^2;
         beta1_ = beta1;
         beta2_ = beta2;
     else
+        k = k + 1;
         m = beta1*m + (1-beta1)*grad;
         v = beta2*v + (1-beta2)*grad.^2;
         beta1_ = beta1_ * beta1;
@@ -31,5 +33,4 @@ function [fin, k, x] = Adam(f, x, a, k, eps, beta1, beta2)
     
     fin = abs(grad) <= 1e-3;
     x = x - a / (sqrt(v_hat) + eps) * m_hat;
-    k = k + 1;
 end
