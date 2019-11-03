faf770daa4a9c3e601bd7c7d23ce54a319a16d7f
yonghwan159 <52481329+yonghwan159@users.noreply.github.com>
2019-11-03 17:00:02 +0900

    Add files via upload

--- newoptimizer.m
+++ newoptimizer.m
@@ -22,7 +22,7 @@ function varargout = newoptimizer(varargin)
 
 % Edit the above text to modify the response to help newoptimizer
 
-% Last Modified by GUIDE v2.5 03-Nov-2019 16:22:53
+% Last Modified by GUIDE v2.5 03-Nov-2019 16:41:36
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -1131,3 +1131,10 @@ function list_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+% --- Executes when figure1 is resized.
+function figure1_SizeChangedFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
